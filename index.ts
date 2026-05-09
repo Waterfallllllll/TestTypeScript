44239fb00a14c4cba0d42051f0631b067a2e39fa
Ilya <ilieturcan424@gmail.com>
2026-05-09 16:29:47 +0300

    Tuples

--- index.ts
+++ index.ts
@@ -7,6 +7,15 @@ const userData = {
     },
 };
 
+const userDataTuple: [...boolean[], number, string] = [true, false, 40, "John"];
+// userDataTuple[3];
+// userDataTuple.push(50);
+// userDataTuple[3];
+
+const res = userDataTuple.map((t) => `${t} - data`);
+
+const [bthd, age, userName] = userDataTuple;
+
 const createError = (msg: string) => {
     throw new Error(msg);
 };
@@ -46,3 +55,34 @@ const nums: number[][] = [
     [3, 5, 6],
     [3, 5, 6],
 ];
+
+type MyNamedTuple = [name: string, age: number, isAdmin: boolean];
+const person: MyNamedTuple = ["John Doe", 30, false];
+
+type MyTuple = [number, string, boolean];
+const myTuple: MyTuple = [10, "Hello", true];
+
+for (const element of myTuple) {
+    console.log(element);
+}
+
+const mappedTuple: MyTuple = myTuple.map((element) => element * 2);
+console.log(mappedTuple);
+
+const filteredTuple: MyTuple = myTuple.filter(
+    (element) => typeof element === "string",
+);
+console.log(filteredTuple);
+
+const reducedValue: number = myTuple.reduce(
+    (acc, curr) => acc + (typeof curr === "number" ? curr : 0),
+    0,
+);
+console.log(reducedValue);
+
+let arr: (string | number)[];
+arr = ["Alex", 2000];
+console.log(arr);
+
+let tup: [string, number];
+tup = ["Alex", 2414];
