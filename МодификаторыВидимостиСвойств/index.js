a02b8e6f48e6dc2390d4da3ce71377ba30e7262d
Ilya <ilieturcan424@gmail.com>
2026-07-02 13:03:33 +0300

    Приватные поля (#, возможность в JS)

--- МодификаторыВидимостиСвойств/index.js
+++ МодификаторыВидимостиСвойств/index.js
@@ -1,41 +1,35 @@
+"use strict";
 class Player {
-    private login: string; // public значение по умолчанию
-    private _password: string; 
-    public server: string;
-    protected consent: boolean; // Приватные свойства и методы не будут видны при наследовании у потомков. Они только для определенного класса. Если всё же хотим использовать, то нужен модификатор protected
-
+    #login; // public значение по умолчанию
+    _password;
+    server;
+    consent; // Приватные свойства и методы не будут видны при наследовании у потомков. Они только для определенного класса. Если всё же хотим использовать, то нужен модификатор protected
     get password() {
         return this._password;
     }
-
-    set password(newPass: string) {
+    set password(newPass) {
         this._password = newPass;
     }
 }
-
+const test = new Player();
+// test.#login
 class CompetitivePlayer extends Player {
-    rank: number;
-
-    private isConsented() {
+    rank;
+    isConsented() {
         this.consent ? "Yes" : "No";
     }
 }
-
 const player = new Player();
 player.password = "1qaz";
-
 // player.login = "asdfsd";
-
 // class User {
 //     public email: string;
 //     public name: string;
-
 //     constructor(email: string, name: string) {
 //         this.email = email;
 //         this.name = name;
 //     }
 // }
-
 // class User {
 //     constructor(public email: string, public name: string) {} // Тоже самое, что и сверху
 // }
