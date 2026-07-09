026807553a183a38812401142286c2285bcfed8c
Ilya <ilieturcan424@gmail.com>
2026-07-09 13:22:39 +0300

    Декораторы в TS

--- Декораторы/index.js
+++ Декораторы/index.js
@@ -1,25 +1,34 @@
 "use strict";
-const myCar = {
-    fuel: "50%",
-    open: true,
-    freeSeats: 3,
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+let myCar = class myCar {
+    fuel = "50%";
+    open = true;
+    freeSeats;
     isOpen() {
         console.log(this.fuel);
         return this.open ? "open" : "close";
     }
 };
-//Декоратор это функция которая может модифицировать объект myCar
-function closeCar(car) {
-    car.open = false;
-    console.log("close car");
-    return car;
-    // Мы задекорировали объект. Такая функция и называется декоратором.
+myCar = __decorate([
+    closeCar
+], myCar);
+function closeCar(constructor) {
+    // constructor.prototype.open = false; // Мы используем prototype потому, что класс это по сути шаблон по которому будет настраиваться объект. Этих свойств и метода ещё по сути не сущетсвует. Они существуют только в прототипе. И только потом, когда мы создадим объект они появятся в объекте. Поэтому мы обращаемся к прототипу и изменяем его значение.
+    return class extends constructor {
+        open = false;
+    };
 }
-function addFuel(car) {
-    car.fuel = "100%";
-    console.log("add fuel");
-    return car;
-}
-// console.log(closeCar(myCar).isOpen()); // Вернёт close. Декоратор правильно отроботал, мы получили модифицированный объект, с которым можем дальше работать
-// f(x(y())) композиция функций. Результат идёт от внутренней к внешней.
-console.log(addFuel(closeCar(myCar)).isOpen());
+const car = new myCar();
+console.log(car.isOpen());
+// function addFuel(car: myCar) {
+//     car.fuel = "100%";
+//     console.log("add fuel");
+//     return car;
+// }
+//# sourceMappingURL=index.js.map
--- Декораторы/index.ts
+++ Декораторы/index.ts
@@ -1,29 +1,35 @@
-const myCar = {
-    fuel: "50%",
-    open: true,
-    freeSeats: 3,
+interface ICar {
+    fuel: string;
+    open: boolean;
+    freeSeats: number;
+}
+
+@closeCar
+class myCar implements ICar {
+    fuel: string = "50%";
+    open: boolean = true;
+    freeSeats: number;
     isOpen() {
         console.log(this.fuel);
         return this.open ? "open" : "close";
     }
 }
 
-//Декоратор это функция которая может модифицировать объект myCar
+function closeCar<T extends {new (...args: any[]): {}}>(constructor: T) {
+    // constructor.prototype.open = false; // Мы используем prototype потому, что класс это по сути шаблон по которому будет настраиваться объект. Этих свойств и метода ещё по сути не сущетсвует. Они существуют только в прототипе. И только потом, когда мы создадим объект они появятся в объекте. Поэтому мы обращаемся к прототипу и изменяем его значение.
 
-function closeCar(car: typeof myCar) {
-    car.open = false;
-    console.log("close car");
-    return car; 
-    // Мы задекорировали объект. Такая функция и называется декоратором.
+    return class extends constructor {
+        open = false;
+    }
 }
 
-function addFuel(car: typeof myCar) {
-    car.fuel = "100%";
-    console.log("add fuel");
-    return car;
-}
+const car = new myCar();
+console.log(car.isOpen());
 
-// console.log(closeCar(myCar).isOpen()); // Вернёт close. Декоратор правильно отроботал, мы получили модифицированный объект, с которым можем дальше работать
+// function addFuel(car: myCar) {
+//     car.fuel = "100%";
+//     console.log("add fuel");
+//     return car;
+// }
 
-// f(x(y())) композиция функций. Результат идёт от внутренней к внешней.
-console.log(addFuel(closeCar(myCar)).isOpen());
+
